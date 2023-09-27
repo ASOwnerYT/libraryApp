503397cdff4a39017215ff6bc0ec124da7541237
ASOwnerYT <asowner@outlook.com>
2023-09-27 15:17:13 +1300

    2 files modified

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-let myLibrary = [theHobbit, new Book('1984', 'Georce Orwell', 304, true)];
+let myLibrary = [
+  new Book('The Hobbit', 'J.R.R. Tolkien', 295, false),
+  new Book('1984', 'George Orwell', 304, true),
+];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -18,8 +20,29 @@ function addBookToLibrary() {
   // TODO
 }
 
-function displayBooks() {
-  // TODO
+function markAsRead(book) {
+  book.read = !book.read;
+  displayBooks();
 }
+
+function displayBooks() {
+  document.querySelector('#myLibrary').innerHTML = myLibrary
+    .map(
+      (book) =>
+        `<article>
+        <h2>${book.title}</h2>
+        <p>${book.author}</p>
+        <p>${book.pages} pages</p>
+        <p>${book.read ? 'Already read' : 'Unread'}</p>
+        <footer>
+          <button onclick="markAsRead(book)">Mark as read</button>
+          <button class="secondary">Remove</button>
+        </footer>
+      </article>`
+    )
+    .join('');
+}
+
+displayBooks();
 
 console.log(theHobbit.info());
